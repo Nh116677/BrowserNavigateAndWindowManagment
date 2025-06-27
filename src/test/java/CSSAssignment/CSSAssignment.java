55947e7107deeafa68db98a652451c62ef83a42b
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-06-27 14:32:30 -0400

    done.

---
+++ src/test/java/CSSAssignment/CSSAssignment.java
@@ -0,0 +1,23 @@
+package CSSAssignment;
+
+public class CSSAssignment {
+
+    public static void main(String[] args) {
+        //Purpose of the Assessment:
+        //
+        //By completing this assessment, students will:
+        //a. Master CSS Selectors: Gain practical experience in using CSS selectors to accurately target and interact with web elements, ensuring precise selection for form input fields.
+        //
+        // User Story
+        //- Title: Form Submission Using Unique CSS Selectors
+        //- As a user,
+        //- I want to navigate to `https://syntaxprojects.com/cssSelector-homework.php`
+        //- So that I can successfully fill the form using appropriate CSS selectors.
+        //
+        //Acceptance Criteria
+        //1. The user should fill in all the text boxes in the form using*CSS selectors that are unique and return only one element (1/1).
+        //
+
+
+    }
+}
