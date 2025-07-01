bb98d3cbb723b24550433a0bfc4a0bb8f411d5dd
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-07-01 12:55:53 -0400

    done.

--- src/test/java/DropDownWithoutSelectTagAssignment/DropDownWithoutSelectTag.java
+++ src/test/java/DropDownWithoutSelectTagAssignment/DropDownWithoutSelectTag.java
@@ -1,8 +1,12 @@
 package DropDownWithoutSelectTagAssignment;
 
 import com.sun.tools.javac.Main;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class DropDownWithoutSelectTag {
 //Purpose of the Assessment:
@@ -49,8 +53,46 @@ public class DropDownWithoutSelectTag {
 
         //1. Choose Your Dream Fruit:
 //   - Retrieve all options from the "Choose your Dream Fruit" dropdown.
-//   - Traverse through the list of options, and select **Mango**.
-//   - Ensure that the selection process is done by iterating through the options and not by directly clicking on the option using Xpath or any direct locator to the item.
-//
+
+        //click to open the dropdown
+        WebElement dreamFruitsDropDown = driver.findElement(By.id("dream_fruits"));
+        dreamFruitsDropDown.click();
+
+        //get all dropdown options
+        List<WebElement> dreamFruitsOptions = driver.findElements(By.cssSelector(".single-dropdown-menu li a"));
+
+        // Traverse through the list of options, and select **Mango**.
+        // Ensure that the selection process is done by iterating through the options and not by directly clicking on the option using Xpath or any direct locator to the item.
+
+        for(WebElement option : dreamFruitsOptions){
+            if (option.getText().equalsIgnoreCase("Mango")){
+                option.click();
+                break;
+            }
+        }
+
+        //2. Choose Your Favorite Hobby:
+        //   - Retrieve all options from the "Choose your Favorite Hobby" dropdown.
+
+        //click to open the dropdrop
+        WebElement yourFavoriteHobbies = driver.findElement(By.id("favorite_hobbies"));
+        yourFavoriteHobbies.click();
+
+        //get all dropdown options
+        List<WebElement> favoriteHobbiesOptions = driver.findElements(By.cssSelector(".multi-dropdown-menu li a"));
+        //   - Traverse through the list and select **Reading**.
+        //   - Ensure that the desired option is selected by iterating through the options, not by directly clicking on it using Xpath or any other direct locator to the item.
+
+        for (WebElement option : favoriteHobbiesOptions){
+            if(option.getText().equalsIgnoreCase("Reading")){
+                option.click();
+                break;
+            }
+        }
+
+
+
+
+
     }
 }
