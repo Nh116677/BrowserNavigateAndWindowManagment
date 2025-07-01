d1897a118f1c963b0ab063fabb46889aee2a15c5
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-06-30 20:21:17 -0400

    done.

---
+++ src/test/java/RadioButtonsAssignment/RadioButtons.java
@@ -0,0 +1,43 @@
+package RadioButtonsAssignment;
+
+public class RadioButtons {
+    //Purpose of the Assessment:
+    //By completing this assessment, students will:
+    //
+    //a. Learn Radio Button Interaction: Gain practical experience in interacting with radio buttons using Selenium, ensuring the correct options are selected in various sections.
+    //
+    //b. Traverse Radio Button Lists: Understand how to retrieve, iterate, and programmatically select specific radio buttons from a list, applying conditional logic to target specific choices.
+    //
+    //c. Handle Disabled and Hidden Elements: Develop skills to interact with disabled and hidden radio buttons by enabling and displaying them dynamically using Selenium commands.
+    //
+    //
+    //
+    //User Story
+    //- Title: Verifying Radio Button Functionality
+    //- As a user,
+    //- I want to navigate to `https://syntaxprojects.com/basic-radiobutton-demo-homework.php`
+    //- So that I can check if all radio buttons are working as expected.
+    //
+    // Acceptance Criteria
+    //
+    //1. Choose Your Favorite Beverage:
+    //   - Select the Juice radio button from the list of favorite beverages.
+    //
+    //2. Preferred Working Environment:
+    //   - Retrieve all radio buttons in the Preferred Working Environment section.
+    //   - Traverse through the list of radio buttons in code and select the Hybrid option.
+    //
+    //
+    //3. Choose Your Favorite Season:
+    //   - If the Spring radio button is disabled, click on the "Enable Buttons" button.
+    //   - Ensure that the Spring option is now enabled, then select it.
+    //   - By default, the Winter radio button is hidden. Ensure, through code, that it is not displayed initially. Then click on the "Show Buttons" button.
+    //   - Verify that the Winter option is displayed after clicking and is available for selection.
+    //
+    //4. Choose Your Favorite Meal:
+    //   - Select the Lunch radio button from the list of favorite meals.
+
+    public static void main(String[] args) {
+        //
+    }
+}
