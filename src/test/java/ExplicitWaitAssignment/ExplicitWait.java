779fbb7e7100e3dc7203959d35ae75933808ecc4
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-07-02 21:30:51 -0400

    done.

--- src/test/java/ExplicitWaitAssignment/ExplicitWait.java
+++ src/test/java/ExplicitWaitAssignment/ExplicitWait.java
@@ -1,14 +1,13 @@
-import org.openqa.selenium.Alert;
+package ExplicitWaitAssignment;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.Callable;
 
 public class ExplicitWait {
     //Purpose of the Assessment:
